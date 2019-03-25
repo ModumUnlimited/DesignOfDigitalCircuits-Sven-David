f0f6e2cec77bc2c2622c51fa317ec97e82cfda2a
David Zollikofer <david@zollikofer.co>
2019-03-25 21:21:56 +0100

    added exercise 1

---
+++ Lab_Reports/Week3/Davids Solution/Exercise1/DisplayChooser.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/24/2019 10:51:49 PM
+// Design Name: 
+// Module Name: DisplayChooser
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DisplayChooser(input [1:0] chooser,output reg [3:0]AN);
+    always @(*)
+        case (chooser) 
+            2'b00: AN = 4'b1110;
+            2'b01: AN = 4'b1101;
+            2'b10: AN = 4'b1011;
+            2'b11: AN = 4'b0111;
+        endcase
+endmodule
---
+++ Lab_Reports/Week3/Davids Solution/Exercise1/topModel.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/22/2019 11:20:41 AM
+// Design Name: 
+// Module Name: topModel
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module topModel(input [3:0]a, input [3:0]b , output overflow, output [0:6]D, output [3:0]AN);
+    // internal wires to connect components
+    wire [4:0] result;
+    wire [3:0] s;
+    
+    assign AN = 4'b1110;
+    
+    // the four bit adder will do the addition
+    FourBitAdder adder(.a(a),.b(b),.s(result));
+    
+    // split result into displayable and overfloe^w
+    assign overflow = result[4];
+    assign s = result[3:0];
+    
+    //give output to displaydriver
+    DisplayDriver disp(.in(s), .D(D));
+endmodule
