c4848e26af2dbd38315dc93f3367129a459d26ce
David Zollikofer <david@zollikofer.co>
2019-04-14 21:20:03 +0200

    files from friday

--- LabSources/Lab6_source_files/lab6_david/ALU.v
+++ LabSources/Lab6_source_files/lab6_david/ALU.v
@@ -11,16 +11,39 @@ ArithmeticUnit arithmetic(.a(a), .b(b), .select(select), .out(outFromArithmetic)
 LogicUnit logic(.a(a), .b(b), .select(select), .out(outFromLogic));
 
 
+
+wire [31:0] resultA;
+
+Adder add(a,b,resultA);
+
+reg [31:0]		slt;       
+reg [31:0]		diff;
+
+always @ ( *)
+    if (select == 4'b1010)
+    begin
+        diff = (a-b);
+        slt = 32'b0;
+        //$display("%b", diff);
+        if(diff[31] == 1'b1)
+              slt = 32'b1;
+        //slt = ((a < b));
+    end		
+
 always @(*)
     begin
         case(select)
-            4'b0000:  out = a + b; //Addition
+            4'b0000: out = resultA; //Addition
             4'b0010: out = a - b; //Substraction
-            4'b1010: out = (a<b)? 32'd1: 32'd0; //SLT
+            4'b1010: out = slt;
             default: out = outFromLogic;
         endcase
     end
     
 assign flag = (out==32'b0)? 1'b1 : 1'b0;
+
+
+
+
 
 endmodule
