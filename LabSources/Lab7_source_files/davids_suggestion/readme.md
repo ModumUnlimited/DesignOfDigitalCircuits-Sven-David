17ffc36e8ff145359ac54cbd76e8f33d1008d217
David Zollikofer <david@zollikofer.co>
2019-04-23 16:29:22 +0200

    lab7 first draft

---
+++ LabSources/Lab7_source_files/davids_suggestion/readme.md
@@ -0,0 +1,3 @@
+# Lab 7 - David's suggestion
+
+It already works like it should, but I am still trying to improve it.
---
+++ LabSources/Lab7_source_files/davids_suggestion/sum.asm
@@ -0,0 +1,26 @@
+#
+# Calculate sum from A to B.
+#
+# Authors: 
+#	X Y, Z Q 
+#
+#
+
+.text
+main:
+	# initialize the two registers
+	add $t0, $zero, $zero
+	addi $t1, $zero, 1000
+	add $t2, $zero, $zero
+	
+loop:	add $t2, $t2, $t0
+	beq $t0, $t1, end
+	addi $t0, $t0, 1
+	j loop
+	
+end:	
+	syscall
+	#j	end	# Infinite loop at the end of the program. 
+
+
+# alternative syscall instead of j end -> simulation will only work this way
